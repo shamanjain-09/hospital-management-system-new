fea9cd848d79e40dab6755686515777d960ac0e9
shamanjain-09 <jainshaman09@gmail.com>
2026-05-23 21:43:47 +0530

    fix cors for production

--- hms-backend/src/main/java/com/hospital/HmsBackendApplication.java
+++ hms-backend/src/main/java/com/hospital/HmsBackendApplication.java
@@ -18,7 +18,7 @@ public class HmsBackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
